19c58df6de77fcce1f1f0a7c06b4f225ae8e51d1
lulwaRashed <looloo--2004@hotmail.com>
2020-06-14 00:49:50 +0300

    Done

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -7,10 +7,10 @@
 
 let name = "Ahmad"
 
-name = "Salem"
+let name2 = "Salem"
 
 
-var myWeight = 60
+var myWeight = 60.0
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -38,37 +38,41 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
+var firstName1 = " sara"
+var lastName1 = " albader"
+var email1 = "sara66@hotmail.com"
+var phoneNumber1 = "11111111"
+var age1 = 16
+var country1 = "bahrain"
+var area1 = "manama"
+var password = "12314"
 var isKuwaiti = false
 
 //Person 2
-
-
-
-
-
+var firstName2 = " maryam"
+var lastName2 = "alfahad"
+var email2 = "maryamal111@hotmail.com"
+var phoneNumber2 = "2222222"
+var age2 = 17
+var country2 = "kuwait"
+var area2 = "qortuba"
+var password2 = "3434345"
+var iskuwaiti2 = true
 
 
 
 
 
 //Person 3
-
-
-
-
-
-
-
-
-
+var firstName3 = "ahmad"
+var lastName3 = "alahmad"
+var email3 = "ahmadall112@hotmail.com"
+var phoneNumber3 = "45453678"
+var age3 = 18
+var country3 = "kuwait"
+var area3 = "surra"
+var password3 = "67676789"
+var iskuwaiti3 = true
 /*
 - - - -
 # المطلوب الثالث
@@ -82,3 +86,29 @@ var isKuwaiti = false
 
  
  */
+age1 == age2
+age2 == age3
+age3 == age1
+area1 == area2
+area3 == area1
+age1 > 18
+age2 > 18
+age3 > 18
+age1 < 18
+age2 < 18
+age3 < 18
+age1 == 17
+age2 == 17
+age3 == 17
+age1 != 17
+age2 != 17
+age3 != 17
+
+ var ages =
+age1 > age2 && age3 > age2
+
+var area =
+area1 == area2 || area3 == area2
+
+var firstNames =
+firstName1 == firstName2 || firstName3 != firstName1
